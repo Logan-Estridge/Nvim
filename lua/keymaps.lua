a8c9bff44eb79f752b26ec09ba7f8188766ae70b
Logan-Estridge <loganestridge@gmail.com>
2024-05-21 17:29:23 -0500

    fixed neoscroll
    removed +zz mappings for <C-u> and <C-d>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,8 +5,6 @@ local options = { noremap = true }
 -- Normal mode mappings
 vim.keymap.set('n', '<leader>h', '<Cmd>nohlsearch<CR>') --<leader>h turns off search highlighting
 vim.keymap.set('n', '<leader>b', '<Cmd>buffers<CR>:b') --<leader>b opens the buffers window
-vim.keymap.set('n', '<C-u>', '<C-u>zz') --<C-u> 1/2 pages up and centers the cursor
-vim.keymap.set('n', '<C-d>', '<C-d>zz') --<C-d> 1/2 pages down and centers the cursor
 vim.keymap.set('n', '<leader>w', '<Cmd>w<CR>') --<leader>w writes the file
 vim.keymap.set('n', '<leader>q', '<Cmd>q<CR>') --<leader>q quits the file
 vim.keymap.set('n', '<localleader>q', '<Cmd>q!<CR>') --<localleader>q quits the file without saving
