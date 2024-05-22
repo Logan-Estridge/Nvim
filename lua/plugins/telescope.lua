9d24125afb4c714eb57a8939cd8b6fc6c7199343
Logan-Estridge <loganestridge@gmail.com>
2024-05-22 08:43:23 -0500

    Improved telescope capabilities
    added capabilities to search vi command history, git files, vi normal
    mode keymappings, and spelling suggestions.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,19 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = '0.1.6',
+    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fG', builtin.git_files, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
+      vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, {})
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
     end
   },
   {
