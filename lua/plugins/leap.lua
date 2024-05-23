11caf7337bff64791182421be626a49c1e90a56d
Logan-Estridge <loganestridge@gmail.com>
2024-05-23 09:36:36 -0500

    added gruvbox, catppuccin, rose-pine. Fixed leap.
    Leap was bound to 'l', very annoying when trying to move right. Changed
    binding to 'm' for 'move to'. Configured gruvbox, catpuccin, and
    rose-pine to automatically change variants based on the vim.o.background
    setting in vim-options.lua. Added a function to change vim.o.background
    based on the time of day in vim-options.lua.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,8 +1,10 @@
 return {
   "ggandor/leap.nvim",
   config = function()
-    vim.keymap.set({'n', 'x', 'o'}, 'l',  '<Plug>(leap-forward)')
-    vim.keymap.set({'n', 'x', 'o'}, 'L',  '<Plug>(leap-backward)')
-    vim.keymap.set({'n', 'x', 'o'}, 'gl', '<Plug>(leap-from-window)')
+    vim.keymap.set({'n', 'x', 'o'}, 'm',  '<Plug>(leap-forward)')
+    vim.keymap.set({'n', 'x', 'o'}, 'M',  '<Plug>(leap-backward)')
+    vim.keymap.set({'n', 'x', 'o'}, 'gm', '<Plug>(leap-from-window)')
   end,
 }
+
+
