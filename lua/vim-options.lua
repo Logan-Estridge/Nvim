91a6c4220a56cccd98c2954ac7bee73f7e6ab2e1
lre0058 <lre0058@cruntch4a.cm.cluster>
2024-05-23 12:14:36 -0500

    Ori branch first commit
    setup solarized colorscheme, removed function to get the time of day and change the vim background accordingly in vim-options.lua

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,23 +14,3 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
-
--- Function to determine the current hour
-local function get_hour()
-  return tonumber(os.date("%H"))
-end
-
--- Function to set the background based on the time of day
-local function set_background()
-  local hour = get_hour()
-  -- Set 'light' background for hours 8AM - 6PM (8 - 18)
-  if hour >= 8 and hour < 18 then
-    vim.o.background = 'light'
-  -- Set 'dark' background for hours 6PM - 8AM (18 - 8)
-  else
-    vim.o.background = 'dark'
-  end
-end
-
--- Call the function to set the background
-set_background()
