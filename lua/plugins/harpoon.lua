759155a1203cbe3834afd3f09447de450552bc4c
Logan-Estridge <loganestridge@gmail.com>
2024-05-25 12:33:20 -0500

    added barbar plugin, changed harpoon keymappings accordingly
    buffers now displayed as tabs, which are clickable. Tab movement is
    based around the alt key.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,16 +8,16 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set("n", "<A-a>", function() harpoon:list():add() end, { desc = "harpoon file"})
-    vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "open harpoon list"})
+    vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end, { desc = "harpoon file"})
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "open harpoon list"})
 
-    vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end, { desc = "move to harpooned file 1"})
-    vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end, { desc =  "move to harpooned file 2"})
-    vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc =  "move to harpooned file 3"})
-    vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc =  "move to harpooned file 4"})
+    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end, { desc = "move to harpooned file 1"})
+    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, { desc =  "move to harpooned file 2"})
+    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, { desc =  "move to harpooned file 3"})
+    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end, { desc =  "move to harpooned file 4"})
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<A-k>", function() harpoon:list():prev() end, { desc = "move to previous harpooned file"})
-    vim.keymap.set("n", "<A-j>", function() harpoon:list():next() end, { desc = "move to next harpooned file"})
+    vim.keymap.set("n", "<C-,>", function() harpoon:list():prev() end, { desc = "move to previous harpooned file"})
+    vim.keymap.set("n", "<C-.>", function() harpoon:list():next() end, { desc = "move to next harpooned file"})
   end
 }
